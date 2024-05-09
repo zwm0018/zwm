a16bbd652fa497a04e4554b32bee04976b2e459b
zwm <2589750729@qq.com>
2024-05-09 21:31:02 +0800

    2-7接口测试

--- demo/src/main/java/com/javaproject/demo/DemoApplication.java
+++ demo/src/main/java/com/javaproject/demo/DemoApplication.java
@@ -5,8 +5,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+@ComponentScan("com.javaproject")
 @SpringBootApplication
 public class DemoApplication {
 	private static final Logger LOG = LoggerFactory.getLogger(DemoApplication.class);
---
+++ demo/src/main/java/com/javaproject/demo/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.javaproject.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    /**
+     *
+     * @return
+     */
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World! Post, " + name;
+    }
+}
